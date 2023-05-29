f3182c44aaa386119f0b03cb23d5b2567f70fcfe
ppquokka <quokka2335@gmail.com>
2023-05-29 23:05:00 +0900

    230529   23:04

---
+++ SSW3sDatatype.java
@@ -0,0 +1,30 @@
+public class SSW3sDatatype {
+        public static void main(String[] args) {
+            
+            try {
+
+                /*float f1 = 35e3f;
+                double d1 = 12E4d;
+                System.out.println(f1);
+                System.out.println(d1);*/  
+                
+                /*boolean isJavaFun = true;
+                boolean isFinishTasty = false;
+                System.out.println(isJavaFun);
+                System.out.println(isFinishTasty);*/
+
+                char myGrade = 'B';
+                System.out.println(myGrade);
+
+                String greeting = "Hello World!";
+                System.out.println(greeting);
+
+            } catch (Exception e) {
+                // TODO: handle exception
+            } finally {
+    
+            }
+            System.out.println();
+            // return 0;
+        }
+    }
