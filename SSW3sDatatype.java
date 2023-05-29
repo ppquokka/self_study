793e13d86bdfc8ab87db52a8d4f2c3946ad15d02
ppquokka <quokka2335@gmail.com>
2023-05-30 01:43:32 +0900

    23.05.20   01:43 SS

--- SSW3sDatatype.java
+++ SSW3sDatatype.java
@@ -13,11 +13,88 @@ public class SSW3sDatatype {
                 System.out.println(isJavaFun);
                 System.out.println(isFinishTasty);*/
 
-                char myGrade = 'B';
+                /*char myGrade = 'B';
                 System.out.println(myGrade);
 
                 String greeting = "Hello World!";
-                System.out.println(greeting);
+                System.out.println(greeting);*/
+
+                /* 
+                //Java Type Casting_Widening Casting : 작은 → 큰
+                // bite → short → char → int → long → float → double
+                int myInt = 9;
+                double mydouble = myInt;
+
+                System.out.println(myInt);
+                System.out.println(mydouble);*/
+
+                /*//    _NArrow Casting : 큰 → 작은
+                // Narrow Casting 시 값 앞의 유형을 ()로 묶어 캐스팅 수동 수행 필요
+                double myDouble = 9.78d;
+                int myInt = (int) myDouble;
+                System.out.println(myDouble);
+                System.out.println(myInt);*/
+
+                /*int x = 100 + 50;
+                System.out.println(x);*/
+
+                /*int sum1 = 100 + 50;
+                int sum2 = sum1 + 250;
+                int sum3 = sum1 + sum2;
+                System.out.println(sum3);*/
+
+                /*int first = 10;
+                ++first;   // first+1
+                int second = 3;
+                second += 3;   // second+3
+                System.out.println(first);
+                System.out.println(second);*/
+
+                /*String firstName = "Jiwon";
+                String lastName = "Kim";
+                System.out.println(firstName +" "+ lastName);
+                // 문자열 붙임
+                System.out.println(firstName.concat(lastName));*/
+
+                /*String first = "10";
+                int second = 20;
+                String third = first + second;
+                System.out.println(third);*/
+
+                /*Math.max(5,10);
+                System.out.println(Math.max(5,10));
+                System.out.println(Math.min(5,10));
+                Math.sqrt(49);
+                System.out.println(Math.sqrt(49));*/
+
+                /*// o부터 샌다
+                String first = "Hello Everybody";
+                System.out.println(first.indexOf("e"));*/
+
+                /*boolean first = true;
+                boolean second = false;
+                System.out.println(first);
+                System.out.println(second);*/
+
+                /*int first = 10;
+                int second = 9;
+                System.out.println( first > second );
+                System.out.println(10>9);
+                System.out.println(first == 10);
+                System.out.println(first == 15);
+                System.out.println( first >= second );*/
+
+                int first = 35;
+                int second = 20;
+                   if (first >= second) {
+                    System.out.println("Much bigger!");
+                   } else {
+                    System.out.println("Noooo");
+                   }
+                   
+
+
+
 
             } catch (Exception e) {
                 // TODO: handle exception
---
+++ SSbj.java
@@ -0,0 +1,24 @@
+public class SSbj {
+        public static void main(String[] args) {
+            
+            try {
+                /*double A = 0;
+                int B = (int)10;
+                System.out.println( A + B );*/
+
+                String joonas = "joonas??!";
+                String baekjoon = "baekjoon??!";
+                boolean jonnas = true;
+                System.out.println(true);
+
+
+                
+            } catch (Exception e) {
+                // TODO: handle exception
+            } finally {
+    
+            }
+            System.out.println();
+            // return 0;
+        }
+    }
