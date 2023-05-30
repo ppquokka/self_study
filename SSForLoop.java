785fe32e955249722633fa3535de74b7b3e4253b
ppquokka <quokka2335@gmail.com>
2023-05-30 18:22:56 +0900

    230530   18:22

---
+++ SSForLoop.java
@@ -0,0 +1,25 @@
+public class SSForLoop {
+        public static void main(String[] args) {
+            
+            try {
+
+                /*for(int first = 0; first < 5; first++){
+                    System.out.println(first);
+                }*/
+
+                for(int alpha=1; alpha<=2; alpha++){
+                    System.out.println("Outer: "+ alpha);
+
+                    for (int beta = 1; beta <= 3; beta++) {
+                        System.out.println("Inner: "+ beta);
+                    }
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+            } finally {
+    
+            }
+            System.out.println();
+            // return 0;
+        }
+    }
--- SSW3sWhileLoop.java
+++ SSW3sWhileLoop.java
@@ -2,11 +2,19 @@ public class SSW3sWhileLoop {
         public static void main(String[] args) {
                 // Java While loop
                 // ????? 나는 2,5나오고 끝인데, 왜 W3는 0,1,2,3,4,가 나오는 것일까?
-                int first = 0;
-                while (first < 5) {
-                    System.out.println(first);
-                    first++;
+                int alpha = 0;
+                while (alpha < 5) {
+                    System.out.println(alpha);
+                    alpha++;
                 }
+                /*int second = 0;
+                do{
+                    System.out.println(second);
+                    second++;
+                }
+                while (second < 5);*/
+
             // return 0;
         }
 }
+        
