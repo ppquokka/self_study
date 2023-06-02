cda678255b9fbcbdba1478fb5e4c5a9f6b59c0f1
ppquokka <quokka2335@gmail.com>
2023-06-02 18:23:15 +0900

    230602     18:23

--- SS_BJ1001.java
+++ SS_BJ1001.java
@@ -3,14 +3,19 @@
  * 입력: 첫째 줄에 A,B주어짐 (0<A, B<10)
  * 출력: 첫째 출에  A-B 출력
 */
+import java.util.Scanner;
 
 public class SS_BJ1001 {      
     public static void main(String[] args) {
         try {
+            Scanner scanner = new Scanner(System.in);
             int A = 0;
             int B = 0;
             int sum = A-B;
-            System.out.println(sum);            
+            System.out.println(sum);  
+            
+            scanner.close();
+            
         } catch (Exception e) {
             // TODO: handle exception
         }
