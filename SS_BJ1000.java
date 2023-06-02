2e74e82a07a13b692b1c9f586ba5df4e5c6e3155
ppquokka <quokka2335@gmail.com>
2023-06-02 14:51:08 +0900

    230602   14:50   백준ss

---
+++ SS_BJ1000.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SS_BJ1000 {   //백준에는 'Main'으로 넣기. 시작은 대문자!!!
+        public static void main(String[] args) {
+            // 1000번  A+B
+            try {
+                Scanner scanner = new Scanner(System.in);
+
+                int A = scanner.nextInt();    // 0 < A
+                int B = scanner.nextInt();    // B < 10
+                int sum = A + B;                   
+                System.out.println(sum);
+
+                scanner.close();
+                
+            } catch (Exception e) {
+                // TODO: handle exception
+            } finally {
+    
+            }
+            // return 0;
+        }
+    }
---
+++ SS_BJ1001.java
@@ -0,0 +1,19 @@
+/*백준에는 파일명 Main으로
+ * 문제: 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램 작성
+ * 입력: 첫째 줄에 A,B주어짐 (0<A, B<10)
+ * 출력: 첫째 출에  A-B 출력
+*/
+
+public class SS_BJ1001 {      
+    public static void main(String[] args) {
+        try {
+            int A = 0;
+            int B = 0;
+            int sum = A-B;
+            System.out.println(sum);            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        // return 0;
+    }
+}
