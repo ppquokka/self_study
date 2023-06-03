e654e6426244c93d77bfa997d98bd33e77d735da
ppquokka <quokka2335@gmail.com>
2023-06-03 18:17:44 +0900

    230603   파일정리   18:17

---
+++ W3School/SSW3sWhileLoop.java
@@ -0,0 +1,21 @@
+package W3School;
+public class SSW3sWhileLoop {
+        public static void main(String[] args) {
+                // Java While loop
+                // ????? 나는 2,5나오고 끝인데, 왜 W3는 0,1,2,3,4,가 나오는 것일까?
+                int alpha = 0;
+                while (alpha < 5) {
+                    System.out.println(alpha);
+                    alpha++;
+                }
+                /*int second = 0;
+                do{
+                    System.out.println(second);
+                    second++;
+                }
+                while (second < 5);*/
+
+            // return 0;
+        }
+}
+        
---
+++ W3School/SSW3shcools.java
@@ -0,0 +1,67 @@
+package W3School;
+public class SSW3shcools {
+    /* w3schools_Java Tutorial_Variables
+      public static void main(String[] args) {
+        //System.out.println("Hello");
+
+        /*System.out.print("Todays weather is ranny!!");
+        System.out.println("So sky is gray color.");
+        System.out.println(1629);
+        System.out.println(350000);
+        System.out.println(3+3);*/
+
+        /*String name = "John";
+        System.out.println(name);
+
+        int myNum = 15;
+        System.out.println(myNum);
+
+        int number;
+        number = 15;
+        System.out.println(number);*/
+
+        /*int myNumm = 15;
+        //System.out.println(myNumm);
+        myNumm =20;
+        System.out.println(myNumm);*/
+
+        /*final int myNum = 15;
+        myNum = 20;*/
+
+        /*String name = "Jiwon";
+        System.out.println("Hello" +", "+ name);*/
+
+        /*String firstName = "Jiwon";
+        String lastName = "Kim";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);*/
+
+        /*int x = 3;
+        int y = 5;
+        System.out.println(x+y);*/
+
+        /*int x = 7, y = 4, z = 2;
+        System.out.println(x+y+z);*/
+
+        /*int x = 7, y = 4, z = 2;
+        int a = x + y + z;
+        System.out.println(a);*/
+
+        /*int x, y, z;
+        x = y = z = 50;
+        System.out.println(x + y + z);*/
+
+        /*int  x, y, z;
+        x = 10;
+        y = 20;
+        z = 30;
+        int a = x + y + z;
+        // System.out.println(x + y + z);
+        System.out.println(a);    
+    }*/
+
+    public static void main(String[] args) {
+
+        
+    }
+}
