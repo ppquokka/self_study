204dfe536882fdf9516948312c98972e73fe18d4
ppquokka <quokka2335@gmail.com>
2023-06-16 17:37:50 +0900

    오늘 푼 문제

---
+++ Programmers_MySQL/MySQL_Lv.1_Untill4.sql
@@ -0,0 +1,23 @@
+/*     23.06.16(금)
+programmers MySQL CodingTest 연습
+Lv.1   4번.어린 동물 찾기 (select)
+*/
+SELECT ANIMAL_ID, NAME               -- column 선택
+FROM ANIMAL_INS                      -- table 선택
+WHERE NOT INTAKE_CONDITION = 'AGED'
+ORDER BY ANIMAL_ID                   -- 정렬 기준 선택
+;
+
+
+/*
+Lv.1   5번.이름이 없는 동물의 아이디 
+*/
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME is NULL            -- = 가 아니라 is
+ORDER BY ANIMAL_ID
+;
+
+
+
+-- ASC, ORDER BY는 기본으로: 오름차순  /  DESC: 내림차순
